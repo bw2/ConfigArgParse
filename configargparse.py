9aa8f7b6de5ebb64fcb497f9519df95c2ae98f51
Tim Zehta <tim@clockwork.net>
2016-01-19 08:56:40 -0600

    added additional test of arg

--- configargparse.py
+++ configargparse.py
@@ -423,7 +423,7 @@ class ArgumentParser(argparse.ArgumentParser):
         # normalize args by converting args like --key=value to --key value
         normalized_args = list()
         for arg in args:
-            if arg[0] in self.prefix_chars and '=' in arg:
+            if arg and arg[0] in self.prefix_chars and '=' in arg:
                 key, value =  arg.split('=', 1)
                 normalized_args.append(key)
                 normalized_args.append(value)
