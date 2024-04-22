5008236569f68389d659ba0277076736063369f1
Yoann Quenach de Quivillic <yquenach@itlink.fr>
2024-04-22 23:50:42 +0200

    Handle pathlib Path used as default_config_files

--- configargparse.py
+++ configargparse.py
@@ -1328,7 +1328,7 @@ class ArgumentParser(argparse.ArgumentParser):
                 if config_arg_string:
                     config_arg_string = "specified via " + config_arg_string
                 if default_config_files or config_arg_string:
-                    msg += " (%s)." % " or ".join(tuple(default_config_files) +
+                    msg += " (%s)." % " or ".join(tuple(map(str, default_config_files)) +
                                                   tuple(filter(None, [config_arg_string])))
                 msg += " " + self._config_file_parser.get_syntax_description()
 
