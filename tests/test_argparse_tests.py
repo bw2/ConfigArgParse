0f60e572498c410bebb9e0e753b29c65556cd7d5
Tim Booth <tim.booth@ed.ac.uk>
2025-04-09 18:46:23 +0100

    Missing import

--- tests/test_argparse_tests.py
+++ tests/test_argparse_tests.py
@@ -1,4 +1,5 @@
 import logging
+import inspect
 from textwrap import dedent as dd
 
 ################################################################################
@@ -8,23 +9,31 @@ from textwrap import dedent as dd
 
 try:
     import test.test_argparse
-    #Sig = test.test_argparse.Sig
-    #NS = test.test_argparse.NS
+
+    # Sig = test.test_argparse.Sig
+    # NS = test.test_argparse.NS
 except ImportError:
-    logging.warning(dd("""\
+    logging.warning(
+        dd(
+            """\
 
         ============================
         WARNING: Many tests couldn't be run because 'import test.test_argparse'
         failed. Try building/installing python from source rather than through
         a package manager.
         ============================
-        """))
+        """
+        )
+    )
 else:
     test_argparse_source_code = inspect.getsource(test.test_argparse)
-    test_argparse_source_code = test_argparse_source_code.replace(
-        'argparse.ArgumentParser', 'configargparse.ArgumentParser').replace(
-        'TestHelpFormattingMetaclass', '_TestHelpFormattingMetaclass').replace(
-        'test_main', '_test_main')
+    test_argparse_source_code = (
+        test_argparse_source_code.replace(
+            "argparse.ArgumentParser", "configargparse.ArgumentParser"
+        )
+        .replace("TestHelpFormattingMetaclass", "_TestHelpFormattingMetaclass")
+        .replace("test_main", "_test_main")
+    )
 
     # pytest tries to collect tests from TestHelpFormattingMetaclass, and
     # test_main, and raises a warning when it finds it's not a test class
@@ -32,24 +41,24 @@ else:
     # prevents pytest from trying.
 
     # run or debug a subset of the argparse tests
-    #test_argparse_source_code = test_argparse_source_code.replace(
+    # test_argparse_source_code = test_argparse_source_code.replace(
     #   "(TestCase)", "").replace(
     #   "(ParserTestCase)", "").replace(
     #   "(HelpTestCase)", "").replace(
     #   ", TestCase", "").replace(
     #   ", ParserTestCase", "")
-    #test_argparse_source_code = test_argparse_source_code.replace(
+    # test_argparse_source_code = test_argparse_source_code.replace(
     #   "class TestMessageContentError", "class TestMessageContentError(TestCase)")
 
     exec(test_argparse_source_code)
 
     # print argparse unittest source code
     def print_source_code(source_code, line_numbers, context_lines=10):
-         for n in line_numbers:
-             logging.debug("##### Code around line %s #####" % n)
-             lines_to_print = set(range(n - context_lines, n + context_lines))
-             for n2, line in enumerate(source_code.split("\n"), 1):
-                 if n2 in lines_to_print:
-                     logging.debug("%s %5d: %s" % (
-                        "**" if n2 == n else "  ", n2, line))
-    #print_source_code(test_argparse_source_code, [4540, 4565])
+        for n in line_numbers:
+            logging.debug("##### Code around line %s #####" % n)
+            lines_to_print = set(range(n - context_lines, n + context_lines))
+            for n2, line in enumerate(source_code.split("\n"), 1):
+                if n2 in lines_to_print:
+                    logging.debug("%s %5d: %s" % ("**" if n2 == n else "  ", n2, line))
+
+    # print_source_code(test_argparse_source_code, [4540, 4565])
