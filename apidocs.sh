9fbeef321983ab0e3ab23956c22d47399550457a
Tim Booth <tim.booth@ed.ac.uk>
2025-04-14 12:08:40 +0100

    See if we can get the API docs to build

--- apidocs.sh
+++ apidocs.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 # This bash script builds the API documentation for ConfigArgParse.
 
-# Resolve source directory path. From https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself/246128#246128
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-cd $SCRIPT_DIR
+# Change to the source directory path.
+cd -P "$( dirname "${BASH_SOURCE}" )"
 
 # Stop if errors
 set -euo pipefail
 IFS=$'\n\t,'
 
 # Figure the project version
-project_version="$(python3 setup.py -V)"
+project_version="$(setuptools-git-versioning)"
 
-# Figure commit ref
+# Figure commit ref or tag
 git_sha="$(git rev-parse HEAD)"
 if ! git describe --exact-match --tags > /dev/null 2>&1 ; then
     is_tag=false
@@ -24,16 +23,17 @@ fi
 # Init output folder
 docs_folder="./apidocs/"
 rm -rf "${docs_folder}"
-mkdir -p "${docs_folder}"
+mkdir -vp "${docs_folder}"
 
-# We generate the docs for the argparse module too, such that we can document 
+# We generate the docs for the argparse module too, such that we can document
 # the methods inherited from argparse.ArgumentParser, not only the methods that configargparse overrides.
 # And it help to have a better vision of the whole thing also.
-curl https://raw.githubusercontent.com/python/cpython/3.9/Lib/argparse.py > ./argparse.py
+curl https://raw.githubusercontent.com/python/cpython/3.13/Lib/argparse.py > ./argparse.py
 echo "__docformat__ = 'restructuredtext'" >> ./argparse.py
 # Delete the file when the script exits
 trap "rm -f ./argparse.py" EXIT
 
+set +e
 pydoctor \
     --project-name="ConfigArgParse ${project_version}" \
     --project-url="https://github.com/bw2/ConfigArgParse" \
@@ -44,6 +44,11 @@ pydoctor \
     --project-base-dir=.\
     --docformat=google \
     --html-output="${docs_folder}" \
-    ./argparse.py ./configargparse.py || true 
+    ./argparse.py ./configargparse.py
 
-echo "API docs generated in ${docs_folder}"
+if [ "$?" = 0 ] ; then
+    printf '\n%s\n' "API docs generated in ${docs_folder}"
+elif [ -e "${docs_folder}/index.html" ] ; then
+    printf '\n%s\n' "API docs generated in ${docs_folder} with errors"
+fi
+
